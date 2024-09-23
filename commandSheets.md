114b5c0f8430aac2b557350e9fba149eeea11a6d
Dev Cyth <hydra@Devs-MacBook-Pro.local>
2024-09-23 14:22:47 +0200

    initial commit

---
+++ commandSheets.md
@@ -0,0 +1,8 @@
+# Commands to run react project using vite 
+
+```bash
+npm create vite@latest
+npm install
+npm run dev
+```
+
---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import User from './User'
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Hello</h1>
+      <User />
+    </>
+  )
+}
+
+
+export default App
